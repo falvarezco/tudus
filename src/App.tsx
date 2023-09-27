130e154c88c42b56cce9385cd44fb483d60a0e4d
Felipe Alvarez <felipeag06@gmail.com>
2023-09-26 19:49:59 -0500

    Some unnecesary code removed and Unit testing config added (along with 1st test)

--- src/App.tsx
+++ src/App.tsx
@@ -29,22 +29,19 @@ function App() {
   const { todos, loading } = useSelector((state: StateType) => state);
   const dispatch = useDispatch();
   const showLoading = todos.length >= 1 && loading;
-  const todosIsEmpty = todos.length === 0;
 
   useEffect(() => {
     dispatch({type: TODOS_REQUESTED});
   }, [dispatch]);
 
-  return (
+  return showLoading ? <h3 style={{textAlign: 'center'}}>..loading</h3>
+  : (
     <div style={styles.container}>
       <h1 style={styles.title}>Tudus</h1>
       <header style={styles.formContainer}>
         <Form />
       </header>
       <ListItems todoItems={todos} />
-      { todosIsEmpty ? <h3 style={{textAlign: 'center'}}>No Tudus to show</h3> : 
-        showLoading && <h3 style={{textAlign: 'center'}}>..loading</h3>
-      }
     </div>
   )
 }
